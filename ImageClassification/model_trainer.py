12d15d399621f3eb4157f7e8270d4f1286fb1b32
sairam.chennapragada@smithsdetection.com <sairam.chennapragada@smithsdetection.com>
2022-01-28 21:36:08 +0530

    Added metrics to tensorboard

--- ImageClassification/model_trainer.py
+++ ImageClassification/model_trainer.py
@@ -3,6 +3,7 @@ import sys
 import time
 import copy
 import glob
+from datetime import datetime
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -15,7 +16,8 @@ import torch.optim as optim
 from torch.optim import lr_scheduler
 import torchvision
 from torchvision import datasets, models, transforms
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, roc_auc_score, precision_score, recall_score,f1_score, log_loss
+
 import torch.nn.functional as F
 from tensorboardX import SummaryWriter
 
@@ -28,10 +30,12 @@ learning_rate=0.001
 momentum=0.9
 step_size=7
 gamma=0.1
-num_epochs=25
+num_epochs=24
 batch_size=5
 mean=np.array([0.45,0.456,0.406])
 std=np.array([0.229, 0.224, 0.225])
+idx_2_class={'0':'ants', '1':'bees', '2':'cats', '3':'dogs'}
+
 
 # Data transformers
 data_transforms={
@@ -57,7 +61,6 @@ dataloaders={x:torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_s
 images, labels = iter(dataloaders['train']).next() # sample input
 dataset_sizes={x:len(image_datasets[x]) for x in sets}
 class_names=image_datasets['train'].classes
-print(class_names)
 
 
 def view_samples(sample_images): 
@@ -66,7 +69,6 @@ def view_samples(sample_images):
     label_font_size=8
     fig=plt.figure()
     for i,sample in enumerate(sample_images):
-        print(sample)
         image=Image.open(sample).resize((256,256),Image.LANCZOS)
         plt.subplot(1, len(sample_images), i+1)
         plt.imshow(image)
@@ -84,10 +86,8 @@ def view_samples(sample_images):
     return
 
 def view_confusionmatrix(predictions, actuals, phase,global_step):  
-    # TODO: Make this dict global
-    class_2_label={'0':'ants', '1':'bees', '2':'cats', '3':'dogs'}
-    predictions=[class_2_label[str(i)] for i in predictions]
-    actuals=[class_2_label[str(i)] for i in actuals]
+    predictions=[idx_2_class[str(i)] for i in predictions]
+    actuals=[idx_2_class[str(i)] for i in actuals]
     cf_matrix = confusion_matrix(predictions, actuals)
     df_cm = pd.DataFrame(cf_matrix, columns=np.unique(actuals), index = np.unique(actuals))    
     df_cm.index.name = 'Actual'
@@ -98,40 +98,42 @@ def view_confusionmatrix(predictions, actuals, phase,global_step):
     # plt.show()
     writer.add_figure(f'ConfusionMatrix/{phase}', fig, global_step=global_step)
     writer.flush()
-    writer.close()
     return
 
+def view_precision_recall_f1(predictions, actuals,phase,epoch):    
+    predictions=[idx_2_class[str(i)] for i in predictions]
+    actuals=[idx_2_class[str(i)] for i in actuals]
+    precision=precision_score(predictions,actuals,average=None)
+    recall=recall_score(predictions, actuals,average=None)
+    f1score=f1_score(predictions, actuals,average=None)
+    writer.add_scalars('Precision',{'ants':precision[0],'bees':precision[1],'cats':precision[2],'dogs':precision[3]}, epoch)
+    writer.add_scalars('Recall',{'ants':recall[0],'bees':recall[1],'cats':recall[2],'dogs':recall[3]}, epoch)
+    writer.add_scalars('f1score',{'ants':f1score[0],'bees':f1score[1],'cats':f1score[2],'dogs':f1score[3]}, epoch)
+    writer.flush()
+    return
 
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     since = time.time()
-
     best_model_wts = copy.deepcopy(model.state_dict())
-    best_acc = 0.0
-
+    best_acc = 0.0    
     for epoch in range(num_epochs):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
-
         # Each epoch has a training and validation phase
         for phase in ['train', 'val']:
             if phase == 'train':
                 model.train()  # Set model to training mode
             else:
                 model.eval()   # Set model to evaluate mode
-
             running_loss = 0.0
-            running_corrects = 0
-            
+            running_corrects = 0            
             prediction_labels=[]
             actual_labels=[]
             # Iterate over data.
             for inputs, labels in dataloaders[phase]:
                 inputs = inputs.to(device)
                 labels = labels.to(device)
-
-
-                # forward
-                # track history if only in train
+                # forward - track history if only in train
                 with torch.set_grad_enabled(phase=='train'):
                     outputs = model(inputs)
                     _, preds = torch.max(outputs, 1)
@@ -156,8 +158,11 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
             epoch_loss = running_loss / dataset_sizes[phase]
             epoch_acc = running_corrects.double() / dataset_sizes[phase]
             print(f"{phase} Loss: {epoch_loss:.4f} | {phase} Accuracy: {epoch_acc:.4f}")
-            view_confusionmatrix(prediction_labels, actual_labels,phase,epoch)
 
+            # Compute classification metrics
+            view_confusionmatrix(prediction_labels, actual_labels,phase,epoch)           
+        
+            
             if phase=='train':
                 scheduler.step()
                 writer.add_scalars('Loss/Train',{'Loss/Train': epoch_loss},epoch)
@@ -167,10 +172,11 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
             elif phase=='val':
                 writer.add_scalars('Loss/Val',{'Loss/Val':epoch_loss},epoch)
                 writer.add_scalars('Accuracy/Val',{'Accuracy/Val':epoch_acc},epoch)
+                view_precision_recall_f1(prediction_labels, actual_labels,phase,epoch)
                 writer.flush()
 
             # deep copy the model
-            if phase == 'val' and epoch_acc > best_acc:
+            if phase == 'val' and epoch_acc > best_acc:               
                 best_acc = epoch_acc
                 best_model_wts = copy.deepcopy(model.state_dict())
 
@@ -183,17 +189,14 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     return model
 
 
-# Convnet as fized feature extractor
-# here, we need to freeze all the network except the final layer
-# we need to set required_grads==False to freeze all the parameters so the gradients are not computed in backward()
+# Freeze all the network except the final layer 
+# Set required_grads==False to freeze all the parameters so the gradients are not computed in backward()
 model=torchvision.models.resnet18(pretrained=True)
 for param in model.parameters():
     param.requires_grad=False
 
 writer.add_graph(model, images)
 writer.flush()
-
-
 
 # Parameters of newly constructed modules have requires_grad=True by default
 num_ftrs=model.fc.in_features
@@ -203,11 +206,27 @@ criterion=nn.CrossEntropyLoss()
 optimizer=optim.SGD(model.fc.parameters(), lr=learning_rate, momentum=momentum)
 scheduler=lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
 
-
-
 # view train samples
 view_samples(sample_images=glob.glob("C:/codebase/Pytorch/sample_data/*.jpg"))
 # Begin train
 model=train_model(model, criterion, optimizer, scheduler, num_epochs=num_epochs)
+# save model
+date = datetime.now().strftime("%Y_%m_%d_%I_%M_%S")
+file_name=f"{date}_bestmodel.pth"
+torch.save(model, f'saved_model/{file_name}')
+# load model
+trained_model=torch.load(f'saved_model/{file_name}')
+trained_model.eval()
+trained_model.to(device)
+
 # Begin Test
+for inputs, labels in dataloaders['val']:
+    inputs = inputs.to(device)
+    outputs = trained_model(inputs)
+    _, preds = torch.max(outputs, 1)
+    final_predictions=list(preds.cpu().detach().numpy())
+    print(final_predictions)
+
+
+
 writer.close()
