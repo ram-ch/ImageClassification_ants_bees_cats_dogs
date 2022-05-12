fad7a2e8b02ee9717dca7083ea1c77e2a17f9f56
ch.sairam1988@gmail.com <ch.sairam1988@gmail.com>
2022-05-12 15:09:25 +0530

    updated readme, added images

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # This is intro about image classification
+In this repo, I have built a classifier for classifying ants, bees, cats and dogs. The classifier is built with transfer learning, using a pretrained CNN architecture/model (vgg16, resnet101). The training is performed using Pytorch frame work.   
+The training data includes:   
+
+| Class       | Train set | Validation set |
+| ----------- | ----------| -------------- |
+| Ants        | 123       | 70             |
+| Bees        | 121       | 83             |
+| Cats        | 125       | 85             |
+| Dogs        | 125       | 80             |
+      
+The data is is just a small subset from all the above mentioned classes
+
+
+<img src="assets/ant.jpg" alt="Ant" width="200" height="200"/>
+<img src="assets/bee.jpg" alt="Bee" width="200" height="200"/>
+<img src="assets/cat.17.jpg" alt="Cat" width="200" height="200"/>
+<img src="assets/dog.12.jpg" alt="Dog" width="200" height="200"/>   
+
+## Data Preprocessing   
+* In the data preprocessing step I have used albumentation library for image augmentation  
+## Model Building  
+* I have used a pretrained ResNet101 from pytorch model zoo and frozen the feature extraction part and retrained the classifier (Transfer Learning)
+## Training & Validation   
+* The training script performs training and validation alternatively in a loop   
+* The training is integrated with Tensorboard   
+<img src="assets/TB_scalars.png" alt="tb" width="500" height="500"/>    <img src="assets/TB_images.png" alt="tb" width="500" height="500"/>   
+
+## Inference    
+* The model with best check point is saved and loaded for inference      
+* If all the class scores are below a threshold of 60% then it is considered as a negative sample   
+<img src="assets/unknown.png" alt="unknown" width="300" height="300"/> <img src="assets/unknown_result.png" alt="tb" width="300" height="300"/>     
+<img src="assets/dog.png" alt="tb" width="300" height="300"/> <img src="assets/dog_result.png" alt="tb" width="300" height="300"/>     
+
+* The results can be further improved by hyper parameter tuning   
+
--- inference.py
+++ inference.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import time
 from datetime import datetime
 import numpy as np
 import pandas as pd
@@ -9,12 +10,12 @@ import torch
 import torchvision
 from torchvision import transforms
 import torch.nn.functional as F
-# from sklearn.metrics import confusion_matrix, roc_auc_score, roc_curve, auc
+import seaborn as sns
 device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # params
 test_image_dir="data/cats_dogs_bees_ants_125/test_samples"
-saved_model="saved_model/2022_05_10_11_51_20_bestmodel.pth"
+saved_model="saved_model/2022_05_12_02_20_23_bestmodel.pth"
 idx_2_class={'0':'ants', '1':'bees', '2':'cats', '3':'dogs'}
 
 
@@ -30,35 +31,57 @@ trained_model=torch.load(saved_model)
 trained_model.eval()
 
 test_images=glob.glob(test_image_dir+"/*.jpg")
-print(test_images)
+
 # create figure
 fig = plt.figure(figsize=(10, 7))
-for i,img in enumerate(test_images):
-    # Load image
-    image = Image.open(img)
-    image = data_transforms(image).float()
-    image = image.clone().detach()
-    image = image.unsqueeze(0)
-    # perform inference
-    print("Performing inference...")
-    output=trained_model(image)
-    prob = F.softmax(output, dim=1)
-    top_p, top_class = prob.topk(4)  
-    # labels and probs
-    top_probs=top_p.detach().numpy()[0]
-    top_classes=top_class.detach().numpy()[0]
-    classes=[idx_2_class[str(i)] for i in top_classes]
-    probabilities=[round(i*100,2) for i in top_probs]
-    results=dict(zip(classes,probabilities))    
-    # show predictions
-    img = plt.imread(img)    
-    plt.imshow(img)
-    plt.axis('off')
-    data="ant: {}% bee: {}% cat: {}% dog: {}%".format(results['ants'],results['bees'],results['cats'],results['dogs'])    
-    plt.text(x=2, y=-3, s=data,c='red',fontsize=10)
+
+def view_result(classes, scores):
+    x_pos = [i for i, _ in enumerate(classes)]
+    plt.bar(x_pos, scores, color='green')
+    plt.xlabel("Classes")
+    plt.ylabel("Scores")
+    plt.title("Prediction")
+    plt.xticks(x_pos, classes)
+    for i in range(len(classes)):
+            plt.text(i,scores[i]+0.5,scores[i], ha = 'center')
     plt.show()
 
 
-# TODO: How to identify negative samples
-# TODO: Add inference time
+
+start_time = time.time()
+# test image
+img=test_images[-2]
+# Load image
+image = Image.open(img)
+image = data_transforms(image).float()
+image = image.clone().detach()
+image = image.unsqueeze(0)
+image = image.to(device)
+# perform inference
+print("Performing inference...")
+output=trained_model(image)
+prob = F.softmax(output, dim=1)
+top_p, top_class = prob.topk(4)  
+# labels and probs
+top_probs=top_p.cpu().detach().numpy()[0]
+top_classes=top_class.cpu().detach().numpy()[0]
+classes=[idx_2_class[str(i)] for i in top_classes]
+probabilities=[round(i*100,2) for i in top_probs]
+results=dict(zip(classes,probabilities))    
+# show image with predictions
+img = plt.imread(img)    
+plt.imshow(img)
+plt.axis('off')
+threshold=60
+print(results)
+print(f"Inference time : {round(time.time() - start_time,2)} secs")
+if any(score > threshold for score in probabilities):
+    data="ant: {}% bee: {}% cat: {}% dog: {}%".format(results['ants'],results['bees'],results['cats'],results['dogs'])  
+else:
+    data="Unknown"
+plt.text(x=2, y=-3, s=data,c='red',fontsize=10)
+plt.show()
+# Show plot of score
+view_result(classes, probabilities)  
+
 # TODO: Try c++ inference script
